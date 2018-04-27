7ff7cbfb711682b89ba9b6d8588a0ab6022608cd
coleSil <cole3789@gmail.com>
2018-04-27 16:30:09 -0400

    Started work on adding team members to a team

---
+++ public_html/scripts/js/membersearch.js
@@ -0,0 +1,8 @@
+// Team Member Search Functionality from teampage.html
+
+$("#member-search").keypress(function(){
+    $.getJSON("/scripts/js/tmp/usersearch.json", function (result){
+        
+    }
+
+})
