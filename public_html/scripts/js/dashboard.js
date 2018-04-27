166d621d954c012b6394d8dc6016d6d9290cc9ac
jlambert23 <jb.lambert23@gmail.com>
2018-04-26 22:44:01 -0400

    events on the calendar

--- public_html/scripts/js/dashboard.js
+++ public_html/scripts/js/dashboard.js
@@ -1,5 +1,16 @@
-$(document).ready(function () {
-  $('#calendar').fullCalendar({
 
-  })
-});
+  $(document).ready(function() {
+
+    $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listWeek'
+      },
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: '/scripts/getEvents.php'
+    });
+
+  });
+
