1d9278187177421deac7de06ce2cc6bd93d83f92
jlambert23 <jb.lambert23@gmail.com>
2018-04-27 00:48:11 -0400

    did some stuff

--- public_html/scripts/js/dashboard.js
+++ public_html/scripts/js/dashboard.js
@@ -1,16 +1,18 @@
+$(document).ready(function () {
 
-  $(document).ready(function() {
-
-    $('#calendar').fullCalendar({
-      header: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'month,agendaWeek,agendaDay,listWeek'
-      },
-      editable: true,
-      eventLimit: true, // allow "more" link when too many events
-      events: '/scripts/getEvents.php'
-    });
-
+  $('#calendar').fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay,listWeek'
+    },
+    editable: true,
+    eventLimit: true, // allow "more" link when too many events
+    events: '/scripts/getEvents.php'
   });
 
+});
+
+$(document).on('mouseenter', '.dropdown-toggle', function () {
+  $(this).trigger('click');
+});
---
+++ public_html/scripts/js/loader.js
@@ -0,0 +1,45 @@
+// Event loader
+$("#event-list").ready(function () {
+  $.getJSON("/scripts/js/tmp/events.json", function (result) {
+    $.each(result, function (i, field) {
+      var dateformat = "dddd, MMM D YYYY";
+      var start = moment(field.start).format("h:mm a " + dateformat);
+      var end = field.hasOwnProperty("end") ? moment(field.end).format("h:mm a " + dateformat) : "";
+
+      // All day conditions
+      if (moment(field.start).format("H:mm") == "0:00") {
+        if (end == "") {
+          start = moment(field.start).format(dateformat);
+        }
+        else if (moment(field.end).format("H:mm") == "0:00") {
+          start = moment(field.start).format(dateformat);
+          end = moment(field.end).format(dateformat);
+        }
+      }
+
+      if (end != "") start += " to ";
+
+      var event = $("<div>").addClass("list-group-item small").appendTo(".list-group");
+      event.append($("<a>", { href: "#" }).append(field.title));
+      event.append($("<div>").append(field.team));
+      event.append($("<div>").append(start));
+      event.append($("<div>").append(end));
+      event.append($("<div>").append(field.location));
+    });
+  });
+});
+
+// Team loader
+$("#team-dropdown").ready(function () {
+  $.getJSON("/scripts/js/tmp/emptyteams.json", function (result) {
+    if (result.length <= 0) {
+      var message = "Click here to create your<br>first team and get started!";
+      $("#team-dropdown").append($("<a>", { href: "#create-team-modal", "data-toggle": "modal", "data-target": "#create-team-modal" }).addClass("dropdown-item").append(message));
+    }
+    else {
+      $.each(result, function (i, field) {
+        $("#team-dropdown").append($("<a>", { href: "#" }).addClass("dropdown-item").append(field.team));
+      });
+    }
+  });
+});
