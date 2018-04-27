c6bf2ddb1fcbc8293b62801ed1f5f924bb430dd6
coleSil <cole3789@gmail.com>
2018-04-27 17:34:17 -0400

    updated membersearch.js

--- public_html/scripts/js/membersearch.js
+++ public_html/scripts/js/membersearch.js
@@ -2,7 +2,12 @@
 
 $("#member-search").keypress(function(){
     $.getJSON("/scripts/js/tmp/usersearch.json", function (result){
-        
-    }
+        $.each(result, function (i, field){
+            
+            var user = $("<div>").append("#member-search");
+            user.append($("<div>").append(field.fName));
+            
+        });
+    });
 
 })
