fb0b4bd9abe4aa5e307ba6e0e804690cd6241bd9
TaharkaZ <coolmojo1234@gmail.com>
2018-04-29 14:16:54 -0400

    deleted two lines in logout

--- public_html/scripts/php/logout.php
+++ public_html/scripts/php/logout.php
@@ -4,7 +4,5 @@ if(isset($_POST['logout'])){
 	session_start();
 	session_unset();
 	session_destroy();
-	echo "/../index.html";
-	header("Location: ../pages/dashboard.html");
 	echo "../../index.html";
 }
