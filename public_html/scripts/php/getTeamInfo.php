ca29e7fee17df029ae09ee434297236c51f2c39c
TaharkaZ <coolmojo1234@gmail.com>
2018-04-29 17:42:03 -0400

    deleted reference code

--- public_html/scripts/php/getTeamInfo.php
+++ public_html/scripts/php/getTeamInfo.php
@@ -1,4 +1,6 @@
 <?php
+	
+	session_start();
 	//given a team id spit back out information as json
 	//change the isset to whatever justin is doing
 	if(isset($_POST['t_id']){
@@ -19,24 +21,16 @@
 	}
 
 	if($row = mysqli_fetch_assoc($result)){
-		/*
-		$hashedPasswordCheck = password_verify($password, $row['password']);
-
-		if($hashedPasswordCheck == false){
-			$json['password'] = false;
-			echo json_encode($json);
-			exit();
-		}
-      */
-
+		
 		$json['team_name'] = $row['team_name'];
 		$json['picture_path'] = $row['picture_path'];
+		echo json_encode($json);
       
 	}
 
 	else{
 		
-	echo "didnt get an ID post...do your job Justin!";
+		echo "didnt get an ID post...do your job Justin!";
 
 	}
 
