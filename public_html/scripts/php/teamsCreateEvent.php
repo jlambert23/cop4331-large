a218dc16ad3df12dc0fb0e2a5d5f2878c628ae00
TaharkaZ <coolmojo1234@gmail.com>
2018-04-29 21:05:59 -0400

    added teamsCreateEvent

--- public_html/scripts/php/teamsCreateEvent.php
+++ public_html/scripts/php/teamsCreateEvent.php
@@ -6,17 +6,24 @@ if(isset($_POST['submit'])){
 	include_once 'dbconnection.php';
 
 	$eventName = mysqli_real_escape_string($conn, $_POST["event-name"]);
-	$startDate = mysqli_real_escape_string($conn, $_POST["start-date-input"]);
-	$startTime = mysqli_real_escape_string($conn, $_POST["start-time-input"]);
-	$endDate = mysqli_real_escape_string($conn, $_POST["end-date-input"]);
-	$endTime = mysqli_real_escape_string($conn, $_POST["end-time-input"]);
+	// start date is now combined with start time
+	$startDate = mysqli_real_escape_string($conn, $_POST["start-date"]);
+
+	
+	//end date is now combined with end time
+	$endDate = mysqli_real_escape_string($conn, $_POST["end-date"]);
 	$eventDescription = mysqli_real_escape_string($conn, $_POST["event-description"]);
 	$eventLocation = mysqli_real_escape_string($conn, $_POST["event-location"]);
+	$eventOwner = $_POST['owner'];
+	
+	//$startTime = mysqli_real_escape_string($conn, $_POST["start-time-input"]);
+	//$endTime = mysqli_real_escape_string($conn, $_POST["end-time-input"]);
+
 
 	//checking to see if any of the feilds have been sent as empty
 	//might need to change this according to Justin's plan. supposed to do error handling on front end?
-	if(empty($eventName) || empty($startDate) || empty($startTime) 
-		|| empty($endDate) || empty($endTime) || empty($eventLocation)
+	if(empty($eventName) || empty($startDate) 
+		|| empty($endDate) || empty($eventLocation)
 		|| (empty($eventDescription)))
 	{
 		//header("Location: ../index.html ? signup=empty");
@@ -24,11 +31,18 @@ if(isset($_POST['submit'])){
 		exit();
 	}
 
+	else if(empty($eventOwner)){
+		echo "Error: either user event or no owner given";
+		exit();
+	}
+
 	//could check for duplicate events or over lappping events.
 	// right now storing in DB without questions
 	else{
 
-	$sql = "INSERT INTO events (name, start_date, end_date, start_time,end_time, location, description) VALUES (?, ?, ?, ?,?,?, ?)";
+
+	//end is a key word in SQL not sure you can insert properly
+	$sql = "INSERT INTO events (name, start, end, location, description, owner) VALUES (?, ?, ?, ?,?,?)";
 		$stmt = mysqli_stmt_init($conn);
 
 		if(!mysqli_stmt_prepare($stmt, $sql)){
@@ -40,29 +54,53 @@ if(isset($_POST['submit'])){
 
 		
 
-		mysqli_stmt_bind_param($stmt, "sssssss" , $eventName, 
-								$startDate , $endDate, $startTime,$endTime, 
-								$eventLocation, $eventDescription);
+		mysqli_stmt_bind_param($stmt, "ssssss" , $eventName, 
+								$startDate , $endDate, 
+								$eventLocation, $eventDescription, $eventOwner);
 
 		mysqli_stmt_execute($stmt);
 
-		//get the Primary Key for the event you just entered
-		$eventId = mysqli_insert_id($conn);
-		$userId = $_SESSION['u_id'] ;
-		//NOW I NEED TO ADD TO THE USER HAS TEAMS TABLE
-
-		$sql = "INSERT INTO users_has_events (users_userID, events_eventID) VALUES( $userId ,$eventId);";
+		//get the Primary Key for the team event you just entered
+		$teamEventId = mysqli_insert_id($conn);
+		$teamId = $_POST['t_id'];
+		
+		//NOW I NEED TO ADD TO THE EVENT HAS TEAMS TABLE
+		$sql = "INSERT INTO event_has_teams (teamID, eventID) VALUES( $teamId ,$teamEventId);";
 
 		mysqli_query($conn,$sql);
 
-		//redirects to the dashboard
+		//redirects to the dashboard SHOULD REDIRECT TO TEAM PAGE
 		header("Location: ../../pages/dashboard.html ? we got in?");
+
+		//PART #3 we have the team PK now we need to query and get all the users in the team
+		// WE need their primary keys from user_has_teams table
+		// put the PK's into an array we need to loop through this array later
+
+		$sql = "SELECT users_userID FROM users_has_teams WHERE teams_teamID = $teamId;";
+
+		 $result = mysqli_query($conn, $sql);
+
+		 //get all the users that belong to that team
+		 while ($row = mysqli_fetch_assoc($result)) {
+           
+           	 //$team['team'] = $row['team_name'];
+             //$json[] = $team;
+
+		 	$userPrimaryKeys[] = $row['users_userID'];
+
+		 	// STEP#4 Now that we have the PK's we need to add them to user_has_event we need the EVENT PK (teamEventId)
+		 	foreach ($userPrimaryKeys as &$currentUserPrimary){
+ 				
+ 				// based on the for loop this should be the PK from the array -> $currentUserPrimary 
+		 		$sql = "INSERT INTO users_has_events (users_userID, teams_teamID) VALUES( $currentUserPrimary,$teamEventId);";
+		 		mysqli_query($conn,$sql);
+			   
+				}
+
+        	}
 
 		}
 	}
-
-
-
 
 }
 
