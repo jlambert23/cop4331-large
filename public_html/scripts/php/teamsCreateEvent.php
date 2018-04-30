024572defb378184c94c855f8958132d83e44096
TaharkaZ <coolmojo1234@gmail.com>
2018-04-30 08:07:38 -0400

    Revert "Revert "Merge branch 'master' into TBranch""
    This reverts commit 77f4ba61f60970b5256d8efa5049e3a903c114fa.

--- public_html/scripts/php/teamsCreateEvent.php
+++ public_html/scripts/php/teamsCreateEvent.php
@@ -1,68 +1,59 @@
 <?php
+
+function teamsCreateEvent() {
 	session_start();
 
-if(isset($_POST['submitEvent'])){
-
-	include_once 'dbconnection.php';
-
-	$eventName = mysqli_real_escape_string($conn, $_POST["event-name"]);
-	// start date is now combined with start time
-	$startD = mysqli_real_escape_string($conn, $_POST["start-date-input"]);
-	$startTime = mysqli_real_escape_string($conn, $_POST["start-time-input"]);
-	$endD = mysqli_real_escape_string($conn, $_POST["end-date-input"]);
-	$endTime = mysqli_real_escape_string($conn, $_POST["end-time-input"]);
-
-	$eventOwner = mysqli_real_escape_string($conn, $_POST["event-team"]);
-
+	if(isset($_SESSION['u_id'])){
 	
-	$startDate = $startD;
-	$startDate .= " ";
-	$startDate .= $startTime;
-
-	$endDate = $endD;
-	$endDate .= " ";
-	$endDate .= $endTime;
+		include_once 'dbconnection.php';
 	
+		$eventName = mysqli_real_escape_string($conn, $_POST["event-name"]);
+		// start date is now combined with start time
+		$startD = mysqli_real_escape_string($conn, $_POST["start-date-input"]);
+		$startTime = mysqli_real_escape_string($conn, $_POST["start-time-input"]);
+		$endD = mysqli_real_escape_string($conn, $_POST["end-date-input"]);
+		$endTime = mysqli_real_escape_string($conn, $_POST["end-time-input"]);
 	
+		
+		$startDate = $startD;
+		$startDate .= " ";
+		$startDate .= $startTime;
 	//end date is now combined with end time
 	$eventDescription = mysqli_real_escape_string($conn, $_POST["event-description"]);
 	$eventLocation = mysqli_real_escape_string($conn, $_POST["event-location"]);
 	
-	//$startTime = mysqli_real_escape_string($conn, $_POST["start-time-input"]);
-	//$endTime = mysqli_real_escape_string($conn, $_POST["end-time-input"]);
-
-
-	//checking to see if any of the feilds have been sent as empty
-	//might need to change this according to Justin's plan. supposed to do error handling on front end?
-	if(empty($eventName) || empty($startDate) 
-		|| empty($endDate) || empty($eventLocation)
-		|| (empty($eventDescription)))
-	{
-		//header("Location: ../index.html ? signup=empty");
-		echo "empty fields\n";
-		exit();
-	}
-
-	else if(empty($eventOwner)){
-		echo "Error: either user event or no owner given";
-		exit();
-	}
-
-	//could check for duplicate events or over lappping events.
-	// right now storing in DB without questions
-	else{
+		$endDate = $endD;
+		$endDate .= " ";
+		$endDate .= $endTime;
 		
-
-
-		//end is a key word in SQL not sure you can insert properly
-		$sql = "INSERT INTO events (name, start, end, location, description, owner) VALUES (?, ?, ?, ?,?,?)";
-		$stmt = mysqli_stmt_init($conn);
-
-		if(!mysqli_stmt_prepare($stmt, $sql)){
-			//echo "SQL error\n";
-			header("Location: ../pages/dashboard.html? eventCreation = ivalidSQL");
+		
+		//end date is now combined with end time
+		$eventDescription = mysqli_real_escape_string($conn, $_POST["event-description"]);
+		$eventLocation = mysqli_real_escape_string($conn, $_POST["event-location"]);
+		$eventOwner = $_POST['event-team'];
+		
+		//$startTime = mysqli_real_escape_string($conn, $_POST["start-time-input"]);
+		//$endTime = mysqli_real_escape_string($conn, $_POST["end-time-input"]);
+	
+	
+		//checking to see if any of the feilds have been sent as empty
+		//might need to change this according to Justin's plan. supposed to do error handling on front end?
+		if(empty($eventName) || empty($startDate) 
+			|| empty($endDate) || empty($eventLocation)
+			|| (empty($eventDescription)))
+		{
+			//header("Location: ../index.html ? signup=empty");
+			echo "empty fields\n";
+			exit();
 		}
-
+	
+		else if(empty($eventOwner)){
+			echo "Error: either user event or no owner given";
+			exit();
+		}
+	
+		//could check for duplicate events or over lappping events.
+		// right now storing in DB without questions
 		else{
 
 
@@ -105,25 +96,33 @@ if(isset($_POST['submitEvent'])){
 			//get all the users that belong to that team
 			while ($row = mysqli_fetch_assoc($result)) {
 			
-				//$team['team'] = $row['team_name'];
-				//$json[] = $team;
-
-				$userPrimaryKeys[] = $row['users_userID'];
-
-				// STEP#4 Now that we have the PK's we need to add them to user_has_event we need the EVENT PK (teamEventId)
-				foreach ($userPrimaryKeys as &$currentUserPrimary){
-					
-					// based on the for loop this should be the PK from the array -> $currentUserPrimary 
-					$sql = "INSERT INTO users_has_events (users_userID, teams_teamID) VALUES( $currentUserPrimary,$teamEventId);";
-					mysqli_query($conn,$sql);
+	
+	
+			//end is a key word in SQL not sure you can insert properly
+			$sql = "INSERT INTO events (name, start, end, location, description, owner) VALUES (?, ?, ?, ?,?,?)";
+			$stmt = mysqli_stmt_init($conn);
+	
+			if(!mysqli_stmt_prepare($stmt, $sql)){
+				//echo "SQL error\n";
+				header("Location: ../pages/dashboard.html? eventCreation = ivalidSQL");
+			}
+	
+			else{
+	
 				
 				}
 
 			}
 
 		}
+	
 	}
-
+	
+	//couldnt get into the if because the session is not working
+	else{
+	
+		echo "denied bitch!!";
+	}
 }
 
 //couldnt get into the if because the session is not working
